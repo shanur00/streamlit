03e8dde9f2db82f7e8ea7d8dcbcef40ad43f42cb
SHANUR MILON <89702734+shanur00@users.noreply.github.com>
2026-05-05 14:09:14 -0700

    Add files via upload

---
+++ strim.py
@@ -0,0 +1,22 @@
+from numpy import place
+from scipy.sparse import data
+import streamlit as st
+import pandas as pd
+import main
+import json
+
+ret = st.chat_input(
+		placeholder="Enter your message",
+		key="chat",
+		max_chars=None,
+		# accept_file="multiple",
+		height="stretch",
+		)
+
+# ret is None when no message submitted, string when user sends message
+if ret:
+	with st.chat_message(name="user"):
+		st.write(ret)
+	main.messages.append({"role": "user", "content": ret})
+	res = main.run_ai()
+	st.write(res)
